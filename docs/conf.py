b2ecb26df056801cff500537c8f83de218df21e2
official-carnival-cruise-help <elenpeter219@gmail.com>
2025-07-01 19:05:32 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'Delta Airlines Helpcenter'
+copyright = '2025'
+author = 'American'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
